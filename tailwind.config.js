1ec8959f0a9f19b77ca0b33b843a5160d3131e61
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-03-18 13:34:29 -0400

    Added some customs styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ export default {
     extend: {
       gridTemplateColumns: {
         // Grid with wrapping for products cards
-        cards: 'repeat(auto-fill, 16rem)',
+        cards: 'repeat(auto-fill, 18rem)',
+      },
+      width: {
+        90: '22.5rem',
+      },
+      height: {
+        // Height for aside menu
+        aside: 'calc(100vh - 4.5rem)',
+      },
+      margin: {
+        17.5: '4.375rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+        '5xl': '2.5rem',
+      },
+      textColor: {
+        'card-color': '#569E67',
+      },
+      borderColor: {
+        'card-color': '#569E67',
+      },
+      backgroundColor: {
+        'card-color': '#569E67',
       },
     },
   },
