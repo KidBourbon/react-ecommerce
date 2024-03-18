ccd3eef5496c7c5f17dfd1f659f1a78ac7726c81
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-03-18 13:36:03 -0400

    Updated Layout design

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,6 +1,6 @@
 function Layout({ children }) {
   return (
-    <div className='flex flex-col items-center mt-20 px-10'>{children}</div>
+    <div className='flex flex-col items-center mt-17.5 px-10'>{children}</div>
   );
 }
 
