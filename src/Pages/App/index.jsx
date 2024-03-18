34094762977d52abcc9ee3e77cc6fb987df1e592
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-03-17 22:27:30 -0400

    Added Navbar and global context to App

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,22 +1,22 @@
-import { useRoutes, BrowserRouter } from "react-router-dom";
+import { useRoutes, BrowserRouter } from 'react-router-dom';
 
-import { Home } from "../Home";
-import { MyAccount } from "../MyAccount";
-import { MyOrder } from "../MyOrder";
-import { MyOrders } from "../MyOrders";
-import { NotFound } from "../NotFound";
-import { SignIn } from "../SignIn";
-
-import "./styles.css";
+import { Home } from '../Home';
+import { MyAccount } from '../MyAccount';
+import { MyOrder } from '../MyOrder';
+import { MyOrders } from '../MyOrders';
+import { NotFound } from '../NotFound';
+import { SignIn } from '../SignIn';
+import { Navbar } from '../../Components/Navbar';
+import { ShoppingCartProvider } from '../../Context';
 
 const AppRoutes = () => {
   const routes = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "/my-account", element: <MyAccount /> },
-    { path: "/my-order", element: <MyOrder /> },
-    { path: "/my-orders", element: <MyOrders /> },
-    { path: "/sign-in", element: <SignIn /> },
-    { path: "/*", element: <NotFound /> },
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
   ]);
 
   return routes;
@@ -24,9 +24,12 @@ const AppRoutes = () => {
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <Navbar />
+        <AppRoutes />
+      </BrowserRouter>
+    </ShoppingCartProvider>
   );
 };
 
