85ca698299b7d0034bca5f60ad3ff4d1601314f4
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-03-17 22:26:46 -0400

    Created Home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,42 @@
-import "./styles.css";
+import { useEffect, useState } from 'react';
 
-function Home() {
-  return <div className="bg-lime-600">Home</div>;
-}
+import { Layout } from '../../Components/Layout';
+import { Card } from '../../Components/Card';
+
+const getProductsUrl = 'https://api.escuelajs.co/api/v1/products';
+
+const formatPrice = price =>
+  Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(price);
+
+const Home = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(getProductsUrl)
+      .then(response => response.json())
+      .then(data => setItems(data));
+  }, []);
+
+  return (
+    <Layout>
+      Home
+      <div className='grid grid-cols-cards justify-center gap-8 w-full max-w-screen-xl'>
+        {items?.map(item => (
+          <Card
+            key={item?.id}
+            title={item?.title}
+            description={item?.description}
+            price={formatPrice(item?.price)}
+            category={item?.category?.name}
+            pictureUrl={item?.images[0]}
+          />
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
 export { Home };
