d77741863d192e2d325cf41f28dae36e4e8e68a7
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-03-25 23:44:08 -0400

    - Updated the calculation of the amount of products in Context. - Total price calculation in Context. - Moved "addProductToCart" method from Card to Context. - Added new operations with products like "increaseProductAmountInCart", "removeProductFromCartOnce" and "removeProductFromCartEntirely" in Context.

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,37 +1,22 @@
 import { useContext } from 'react';
 
 import { GlobalContext } from '../../Context';
+import { onImageLoadError, formatPrice } from '../../Utils';
 
-import defaultImg from './no-image.jpg';
 import './styles.css';
 
-const onImgLoadError = event => {
-  event.target.src = defaultImg;
-  event.onerror = null;
-};
-
-const Card = ({ title, description, category, price, pictureUrl }) => {
+const Card = ({ id, title, description, category, price, pictureUrl }) => {
   const {
-    cartProducts,
-    setCartProducts,
     openProductDetail,
     closeProductDetail,
     openCheckoutSideMenu,
+    closeCheckoutSideMenu,
     setItemToShow,
-    cardWasClickedRef,
-    cardButtonWasClickedRef,
+    addProductToCart,
   } = useContext(GlobalContext);
 
-  const updateCardWasClickedRef = wasClicked => {
-    cardWasClickedRef.current = wasClicked;
-  };
-
-  const updateCardButtonWasClickedRef = wasClicked => {
-    cardButtonWasClickedRef.current = wasClicked;
-  };
-
-  const onClickCard = () => {
-    updateCardWasClickedRef(true);
+  const onClickCard = event => {
+    event.stopPropagation();
     setItemToShow({
       title,
       description,
@@ -39,42 +24,30 @@ const Card = ({ title, description, category, price, pictureUrl }) => {
       price,
       pictureUrl,
     });
+    closeCheckoutSideMenu();
     openProductDetail();
   };
 
-  const addProductToCart = () => {
-    setCartProducts([
-      ...cartProducts,
-      {
-        title,
-        description,
-        category,
-        price,
-        pictureUrl,
-      },
-    ]);
-  };
-
-  const onClickButton = () => {
-    updateCardButtonWasClickedRef(true);
-    addProductToCart();
+  const onClickButton = event => {
+    event.stopPropagation();
+    addProductToCart({ id, title, description, category, price, pictureUrl });
     closeProductDetail();
     openCheckoutSideMenu();
   };
 
   return (
     <div className='layer-1 w-72 h-96 bg-transparent'>
-      <div className='layer-2 w-full h-full rounded-5xl bg-[#00d664] cursor-pointer transition-all duration-300'>
+      <div
+        className='layer-2 w-full h-full rounded-5xl bg-[#00d664] cursor-pointer transition-all duration-300'
+        onClick={onClickCard}
+      >
         <div className='layer-3 flex flex-col w-full h-full border-2 border-solid border-card-color bg-zinc-950 transition-all duration-200'>
-          <figure
-            className='flex-grow relative w-full h-[37%]'
-            onClick={onClickCard}
-          >
+          <figure className='flex-grow relative w-full h-[37%]'>
             <img
               className='card-img w-full h-full p-0.5 object-cover text-white transition-all duration-200'
               src={pictureUrl}
               alt={description}
-              onError={onImgLoadError}
+              onError={onImageLoadError}
             />
 
             <span className='absolute bottom-0 left-0 ml-4 mb-4 px-2 py-0.5 rounded-lg text-zinc-950 text-sm bg-white/60'>
@@ -82,12 +55,9 @@ const Card = ({ title, description, category, price, pictureUrl }) => {
             </span>
           </figure>
 
-          <p
-            className='flex flex-col justify-between items-center gap-1 pt-1 pb-2 px-2 text-center'
-            onClick={onClickCard}
-          >
+          <p className='flex flex-col justify-between items-center gap-1 pt-1 pb-2 px-2 text-center'>
             <span className='text-lg font-bold text-card-color'>{title}</span>
-            <span className='font-bold text-white'>{price}</span>
+            <span className='font-bold text-white'>{formatPrice(price)}</span>
           </p>
 
           <button
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,16 +1,26 @@
-import { createContext, useEffect, useRef, useState } from 'react';
+import { createContext, useRef, useState } from 'react';
 
 const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
   // All products in store
-  const [items, setItems] = useState([]);
+  const [products, setProducts] = useState([]);
 
   // Products in cart
   const [cartProducts, setCartProducts] = useState([]);
 
   // Shopping cart counter
-  const count = cartProducts.length;
+  const count = cartProducts.reduce((totalAmount, product) => {
+    return totalAmount + product.amount;
+  }, 0);
+
+  // Total price of all products in the shopping cart
+  const totalPrice = cartProducts.reduce((totalPrice, product) => {
+    return totalPrice + product.price * product.amount;
+  }, 0);
+
+  // Shopping cart orders
+  const [orders, setOrders] = useState([]);
 
   // Open/Close Product Detail menu
   const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
@@ -25,19 +35,83 @@ const GlobalProvider = ({ children }) => {
   // Item to show in the Product Detail menu
   const [itemToShow, setItemToShow] = useState({});
 
-  const cardWasClickedRef = useRef(false);
-  const cardButtonWasClickedRef = useRef(false);
+  // References that store if some components have been clicked on
   const productDetailWasClickedRef = useRef(false);
   const checkoutSideMenuWasClickedRef = useRef(false);
+
+  const increaseProductAmountInCart = id => {
+    let isProductInCart = false;
+
+    const newCartProducts = cartProducts.map(product => {
+      if (product.id === id) {
+        isProductInCart = true;
+
+        return {
+          ...product,
+          amount: product.amount + 1,
+        };
+      }
+
+      return product;
+    });
+
+    return { newCartProducts, isProductInCart };
+  };
+
+  const addProductToCart = ({ id, title, description, category, price, pictureUrl }) => {
+    const { newCartProducts, isProductInCart } = increaseProductAmountInCart(id);
+
+    if (isProductInCart) {
+      setCartProducts(newCartProducts);
+    } else {
+      setCartProducts([
+        ...cartProducts,
+        {
+          id,
+          title,
+          description,
+          category,
+          price,
+          pictureUrl,
+          amount: 1,
+        },
+      ]);
+    }
+  };
+
+  const removeProductFromCartOnce = id => {
+    const newCartProducts = [];
+
+    for (const product of cartProducts) {
+      if (product.id === id) {
+        if (product.amount > 1) {
+          newCartProducts.push({
+            ...product,
+            amount: product.amount - 1,
+          });
+        }
+      } else newCartProducts.push(product);
+    }
+
+    setCartProducts(newCartProducts);
+  };
+
+  const removeProductFromCartEntirely = id => {
+    const newCartProducts = cartProducts.filter(product => product.id !== id);
+    setCartProducts(newCartProducts);
+  };
 
   return (
     <GlobalContext.Provider
       value={{
+        products,
+        setProducts,
         cartProducts,
         setCartProducts,
         count,
-        items,
-        setItems,
+        totalPrice,
+        orders,
+        setOrders,
         isProductDetailOpen,
         openProductDetail,
         closeProductDetail,
@@ -46,10 +120,11 @@ const GlobalProvider = ({ children }) => {
         closeCheckoutSideMenu,
         itemToShow,
         setItemToShow,
-        cardWasClickedRef,
-        cardButtonWasClickedRef,
         productDetailWasClickedRef,
         checkoutSideMenuWasClickedRef,
+        addProductToCart,
+        removeProductFromCartOnce,
+        removeProductFromCartEntirely,
       }}
     >
       {children}
