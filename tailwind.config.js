4b0dd7dfac28127b36aaf70ebed48d5738c1d6b9
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-03-25 23:17:45 -0400

    - New values for height and margin - New plugin for multiline text with ellipsis - New plugin to disable text selection

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ export default {
       height: {
         // Height for aside menu
         aside: 'calc(100vh - 4.5rem)',
+        navbar: '4.375rem',
+      },
+      margin: {
+        navbar: '4.375rem',
       },
       borderRadius: {
         '4xl': '2rem',
@@ -35,15 +39,31 @@ export default {
   plugins: [
     function ({ addUtilities }) {
       const newUtilities = {
-        /* Hide scrollbar for Chrome, Safari and Opera */
+        // Hide scrollbar for Chrome, Safari and Opera
         '.no-scrollbar::-webkit-scrollbar': {
           display: 'none',
         },
 
-        /* Hide scrollbar for IE, Edge and Firefox */
+        // Hide scrollbar for IE, Edge and Firefox
         '.no-scrollbar': {
           '-ms-overflow-style': 'none' /* IE and Edge */,
           'scrollbar-width': 'none' /* Firefox */,
+        },
+
+        // Multiline text with ellipsis
+        '.multiline-ellipsis': {
+          overflow: 'hidden',
+          display: '-webkit-box',
+          '-webkit-box-orient': 'vertical',
+          '-webkit-line-clamp': '3' /* start showing ellipsis when 3rd line is reached */,
+          'white-space': 'pre-wrap' /* let the text wrap preserving spaces */,
+        },
+
+        // Disable text selection
+        '.prevent-select': {
+          '-webkit-user-select': 'none' /* Safari */,
+          '-ms-user-select': 'none' /* IE 10 and IE 11 */,
+          'user-select': 'none' /* Standard syntax */,
         },
       };
 
