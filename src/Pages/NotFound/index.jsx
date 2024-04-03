af1d6cef1f3e2cabbef47d3d43d2b03a30a1e72f
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-04-03 18:28:54 -0400

    Updated text size

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -3,7 +3,7 @@ import { Layout } from '../../Components/Layout';
 function NotFound() {
   return (
     <Layout>
-      <h1 className='mb-6 text-4xl font-medium text-center text-white'>Error 404: Not Found!</h1>
+      <h1 className='mb-6 text-3xl font-medium text-center text-white'>Error 404: Not Found!</h1>
     </Layout>
   );
 }
