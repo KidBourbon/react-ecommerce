3fe10ca7115afd9c0208360826cafb2bec34a220
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-04-03 18:28:58 -0400

    Updated text size

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -3,7 +3,7 @@ import { Layout } from '../../Components/Layout';
 function SignIn() {
   return (
     <Layout>
-      <h1 className='mb-6 text-4xl font-medium text-center text-white'>
+      <h1 className='mb-6 text-3xl font-medium text-center text-white'>
         You are logged in as KidBourbon!
       </h1>
     </Layout>
